21fa024e716d6aac3e25be5bee34fcd5bf3cba10
Avro Shaheen <avrostu@gmail.com>
2024-12-18 23:44:50 +0600

    React router dom added and Mainlayout created

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -31,7 +31,7 @@ const JobListing = ({ job }) => {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3 flex items-center">
-            <FaMapMarkerAlt />
+            <FaMapMarkerAlt className="mr-1" />
             {job.location}
           </div>
           <a
