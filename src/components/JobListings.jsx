bc6ccb904656b67f3d9f5d8913f3953e68499927
Avro Shaheen <avrostu@gmail.com>
2024-12-22 20:41:14 +0600

    Minor UI Changes

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,8 +1,13 @@
-import jobs from "../jobs.json";
+import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
 import JobListing from "./JobListing";
 
 const JobListings = ({ isHome = false }) => {
+  const [jobs, setJobs] = useState();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {}, []);
   const recentJobs = isHome ? jobs.slice(0, 3) : jobs;
+
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -17,6 +22,9 @@ const JobListings = ({ isHome = false }) => {
       </div>
     </section>
   );
+};
+JobListings.propTypes = {
+  isHome: PropTypes.bool,
 };
 
 export default JobListings;
