53e36bd3ff7d23fb3ecdad2e3aa52bde2ffef070
avroshaheen <avrostu@gmail.com>
2025-01-25 00:34:48 +0600

    Components Updated

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000, // React app runs on port 3000
+    port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000", // Proxy API requests to the Express server
-        changeOrigin: true, // Ensures proper forwarding of requests
-        rewrite: (path) => path.replace(/^\/api/, ""), // Strip '/api' when forwarding
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
